7d2f8b2445e4ddffff9db097b36425acb6a7f900
Alcides <alcidescon@gmail.com>
2025-05-02 13:02:59 +0200

    Definiendo rutas, modelos y el panel de admin

--- djangoproject/myapp/urls.py
+++ djangoproject/myapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
 	path('', views.index, name='hello'),
     path('about/', views.about, name='about'),
 	path('hello/<str:username>', views.hello, name='hello'),
-    
+	path('projects/', views.projects, name='projects'),
+	path('tasks/<str:title>', views.tasks, name='tasks'),
 ]
--- djangoproject/myapp/views.py
+++ djangoproject/myapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
@@ -9,3 +12,11 @@ def hello(request, username):
 
 def about(request):
     return HttpResponse('<h1>About</h1>')
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def tasks(request, title):
+    task = Task.objects.get(title=title)
+    return HttpResponse('Task: %s' % task.title)
